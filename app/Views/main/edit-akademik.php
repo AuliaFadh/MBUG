7d61c3aa8a6cdaff56cb01ef5e886279018fedfc
TarmanaM <isatarmana@gmail.com>
2023-07-31 08:09:23 +0700

    v5.3.2 FE fix Laporan Akademik Admin

--- app/Views/main/edit-akademik.php
+++ app/Views/main/edit-akademik.php
@@ -36,7 +36,7 @@
                                     <div class="container1 custom-container-form col-lg-12 col-md-12 col-sm-12 ">
                                         <label class="label-form">NPM</label>
                                         <div style="display: block;" class=" col-lg-12 col-md-12 col-sm-12">
-                                            <input readonly value="{diisi dari database}" type="text" class="form-control custom-textfield col-lg-3 col-md-3 col-sm-3">
+                                            <input readonly value="{10120544}" type="text" class="form-control custom-textfield col-lg-7 col-md-5 col-sm-3">
                                             <div class=" invalid-feedback">
 
                                             </div>
@@ -62,18 +62,27 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="container1 custom-container-form col-lg-12 col-md-12 col-sm-12 ">
+                                    <div class="container1  custom-container-form col-lg-12 col-md-12 col-sm-12 ">
                                         <label class="label-form">Jenis Beasiswa</label>
                                         <div style="display: block;" class=" col-lg-12 col-md-12 col-sm-12">
-                                            <input readonly value="{diisi dari database}" type="text" class="form-control custom-textfield ">
-                                            <div class=" invalid-feedback">
+                                            <input type="text" value="{diisi dari database}" id="jb-input" class="form-control custom-textfield ">
+                                            <div id="jb-search" class="dropdown-custom col-lg-12 col-md-12 col-sm-12" style="display: none;">
+
+                                                <a id="jb-data" onclick="fillInputJB('Dicoding Indonesia')">Dicoding Indoneisa </a>
+                                                <a id="jb-data" onclick="fillInputJB('KIPK')">KIPK </a>
+                                                <a id="jb-data" onclick="fillInputJB('Kementrian Pertahanan Indonesia')">Kementrian Pertahanan Indonesia</a>
+
+                                                <span id="jb-noData" style="display: none;">Data tidak ada</span>
+                                            </div>
+                                            <div class="invalid-feedback">
 
                                             </div>
                                         </div>
                                     </div>
+                                   
                                     <div class="container1 custom-container-form col-lg-12 col-md-12 col-sm-12 ">
                                         <label class="label-form">Semester</label>
-                                        <div style="display: block;" class=" col-lg-2 col-md-2 col-sm-2">
+                                        <div style="display: block;" class=" col-lg-2 col-md-3 col-sm-5">
                                             <input value="-99" type="number" min=1 max=14 class="form-control custom-textfield ">
                                             <div class=" invalid-feedback">
 
@@ -84,14 +93,14 @@
                                         <label class="label-form" style="margin-right: 20px;">Tahun Ajaran</label>
 
 
-                                        <select class="form-control  custom-textfield col-lg-2 col-md-2 col-sm-2">
+                                        <select class="form-control  custom-textfield col-lg-3 col-md-4 col-sm-6">
                                             <option></option>
                                             <option value="2018">ATA</option>
                                             <option value="2018">PTA</option>
                                         </select>
-                                        <input required type="number" class="form-control custom-textfield col-lg-2 col-md-2 col-sm-2">
+                                        <input required type="number" class="form-control custom-textfield col-lg-3 col-md-3 col-sm-4">
                                         <h3>/</h3>
-                                        <input required type="number" class="form-control custom-textfield col-lg-2 col-md-2 col-sm-2">
+                                        <input required type="number" class="form-control custom-textfield col-lg-3 col-md-3 col-sm-4">
                                     </div>
 
 
@@ -99,17 +108,17 @@
 
                                     <div class="container1 custom-container-form col-lg-12 col-md-12 col-sm-12 ">
                                         <label class="label-form">IPK</label>
-                                        <div style="display: block;" class=" col-lg-12 col-md-12 col-sm-12">
-                                            <input value="-99" min=0 max=4 step=0.1 type="number" class="form-control custom-textfield col-lg-2 col-md-2 col-sm-2">
+                                        <div style="display: block;" class=" col-lg-2 col-md-4 col-sm-5">
+                                            <input value="-99" min=0 max=4 step=0.01 type="number" class="form-control custom-textfield ">
                                             <div class=" invalid-feedback">
 
                                             </div>
                                         </div>
                                     </div>
                                     <div class="container1 custom-container-form col-lg-12 col-md-12 col-sm-12 ">
-                                        <label class="label-form">IPK Lokal</label>
-                                        <div style="display: block;" class=" col-lg-12 col-md-12 col-sm-12">
-                                            <input value="-99" min=0 max=4 step=0.1 type="number" class="form-control custom-textfield col-lg-2 col-md-2 col-sm-2">
+                                        <label class="label-form">IPK Local</label>
+                                        <div style="display: block;" class=" col-lg-2 col-md-4 col-sm-5">
+                                            <input value="-99" min=0 max=4 step=0.01 type="number" class="form-control custom-textfield ">
                                             <div class=" invalid-feedback">
 
                                             </div>
@@ -117,15 +126,14 @@
                                     </div>
                                     <div class="container1 custom-container-form col-lg-12 col-md-12 col-sm-12 ">
                                         <label class="label-form">IPK UU</label>
-                                        <div style="display: block;" class=" col-lg-12 col-md-12 col-sm-12">
-                                            <input value="-99" min=0 max=4 step=0.1 type="number" class="form-control custom-textfield col-lg-2 col-md-2 col-sm-2">
+                                        <div style="display: block;" class=" col-lg-2 col-md-4 col-sm-5">
+                                            <input value="-99" min=0 max=4 step=0.01 type="number" class="form-control custom-textfield ">
                                             <div class=" invalid-feedback">
 
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-lg-12 col-md-12 col-sm-12">
-
                                         <div class="form-group fallback w-100 ">
                                             <label class="label-form">Rangkuman Nilai</label>
                                             <a style=" margin-left :15px" title="Lihat Dokumen Sebelumnya" href="pdf/pdf1.pdf"><img id="doc-search" class="btn btn-sm btn-success" src="<?= base_url('asset/img/doc-search.png'); ?>" alt=""></a>
--- app/Views/main/tambah-akademik.php
+++ app/Views/main/tambah-akademik.php
@@ -1,54 +1,43 @@
 <?= $this->extend('layout/web-MBUG-admin'); ?>
 <?= $this->section('content') ?>
 <div class="content-body">
-    <!-- row -->
     <div class="container-fluid">
-
         <div class="col-sm-6 p-md-0  mt-2 mt-sm-0 d-flex">
             <ol class="breadcrumb">
-                <!-- Ubah disini -->
                 <li class="breadcrumb-item"><a href="/admin/home">
-                        <img class="logo-abbr logo-home" src="<?= base_url('asset/img/Home.png'); ?>" alt="">
-                        Dashboard</a></li>
+                        <img class="logo-abbr logo-home" src="<?= base_url('asset/img/Home.png'); ?>" alt="">Dashboard</a></li>
                 <li class="breadcrumb-item active"><a href="/admin/akademik">Laporan Akademik</a></li>
                 <li class="breadcrumb-item active"><a href="/admin/akadmik/add">Tambah Akademik</a></li>
-
-
             </ol>
         </div>
 
         <div class="row">
-            <!-- Ubah disini -->
             <div class="col-lg-12">
                 <div class="card">
                     <div class="card-header">
                         <div class="container1">
-
                             <h3>Tambah Akademik</h3>
                         </div>
-
                     </div>
+
                     <div class="card-body">
 
                         <form action="" method="">
                             <div class="row">
                                 <div class="col-lg-8 col-md-8 col-sm-8">
+
                                     <div class="container1 row custom-container-form col-lg-12 col-md-12 col-sm-12 ">
                                         <label class="label-form">NPM</label>
-                                        <div style="display: block;" class=" col-lg-3 col-md-5 col-sm-3">
-                                            <input type="text" id="npm-input" class="form-control custom-textfield ">
-                                            <div id="npm-drop-down" class="card-body  custom-textfield col-lg-12 col-md-12 col-sm-7" style="display: none;">
-                                                <span id="npm">10120544 </span>
-                                                <span id="npm">102322 </span>
-                                                <span id="npm">1203244 </span>
-                                                <span id="npm">1203214</span>
-                                                <span id="npm">1203244</span>
-                                                <span id="npm">12042244</span>
-                                                <span id="npm">1203244</span>
-                                                <span id="npm">113244</span>
-                                                <span id="npm">1203244</span>
-                                                <span id="npm">1323244</span>
-                                                <span id="npm">13213244</span>
+                                        <div style="display: block;" class=" col-lg-8 col-md-12 col-sm-12">
+                                            <input type="text" id="npm-input" class="form-control custom-textfield col-lg-7 col-md-5 col-sm-3">
+                                            <div id="npm-search" class="dropdown-custom col-lg-8 col-md-8 col-sm-8" style="display: none;">
+                                                <!-- For each -->
+                                                <a id="npm-data" onclick="fillInputNPM('10120698','Aulia','Sistem Informasi')">10120698 </a>
+                                                <a id="npm-data" onclick="fillInputNPM('10120544','M Aulia','Sistem Informasi')">10120693128 </a>
+                                                <a id="npm-data" onclick="fillInputNPM('10120328','B Aulia','Sistem Informasi')">1012032 </a>
+                                                <!-- end foreach -->
+                                                
+
                                                 <span id="npm-noData" style="display: none;">Data tidak ada</span>
                                             </div>
                                             <div class="invalid-feedback">
@@ -56,22 +45,38 @@
                                             </div>
                                         </div>
                                     </div>
+
+                                    <div class="container1 row custom-container-form col-lg-12 col-md-12 col-sm-12 ">
+                                        <label class="label-form">Nama</label>
+                                        <div style="display: block;" class=" col-lg-8 col-md-12 col-sm-12">
+                                            <input type="text" readonly id="npm-name" class="form-control custom-textfield ">
+                                            <div class="invalid-feedback">
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="container1 row  custom-container-form col-lg-12 col-md-12 col-sm-12 ">
+                                        <label class="label-form">Program Studi</label>
+                                        <div style="display: block;" class=" col-lg-8 col-md-12 col-sm-12">
+                                            <input type="text" readonly id="npm-ps" class="form-control custom-textfield ">
+                                            <div class="invalid-feedback">
+
+                                            </div>
+                                        </div>
+                                    </div>
+
                                     <div class="container1 row custom-container-form col-lg-12 col-md-12 col-sm-12 ">
                                         <label class="label-form">Jenis Beasiswa</label>
-                                        <div style="display: block;" class=" col-lg-8 col-md-10 col-sm-8">
+                                        <div style="display: block;" class=" col-lg-8 col-md-12 col-sm-12">
                                             <input type="text" id="jb-input" class="form-control custom-textfield ">
-                                            <div id="jb-drop-down" class="card-body  custom-textfield col-lg-12 col-md-12 col-sm-7" style="display: none;">
-                                                <span id="jb">Kartu Indonesia Pintar </span>
-                                                <span id="jb">DIcoding </span>
-                                                <span id="jb">Bangkit </span>
-                                                <span id="jb">1203214</span>
-                                                <span id="jb">1203244</span>
-                                                <span id="jb">12042244</span>
-                                                <span id="jb">1203244</span>
-                                                <span id="jb">113244</span>
-                                                <span id="jb">1203244</span>
-                                                <span id="jb">1323244</span>
-                                                <span id="jb">13213244</span>
+                                            <div id="jb-search" class="dropdown-custom col-lg-8 col-md-8 col-sm-7" style="display: none;">
+
+                                                <a id="jb-data" onclick="fillInputJB('Dicoding Indonesia')">Dicoding Indoneisa </a>
+                                                <a id="jb-data" onclick="fillInputJB('KIPK')">KIPK </a>
+                                                <a id="jb-data" onclick="fillInputJB('Kementrian Pertahanan Indonesia')">Kementrian Pertahanan Indonesia</a>
+
                                                 <span id="jb-noData" style="display: none;">Data tidak ada</span>
                                             </div>
                                             <div class="invalid-feedback">
@@ -79,6 +84,7 @@
                                             </div>
                                         </div>
                                     </div>
+
                                     <div class="container1 row custom-container-form col-lg-12 col-md-12 col-sm-12 ">
                                         <label class="label-form">Semester</label>
                                         <div style="display: block;" class=" col-lg-2 col-md-3 col-sm-5 ">
@@ -97,9 +103,9 @@
                                                     <option value="2018">PTA</option>
                                                     <option value="2018">ATA</option>
                                                 </select>
-                                                <input required min="1981" type="number" class="form-control custom-textfield col-lg-3 col-md-4 col-sm-4">
+                                                <input required min="1981" type="number" class="form-control custom-textfield col-lg-3 col-md-3 col-sm-4">
                                                 <h3>/</h3>
-                                                <input required min="1982" type="number" class="form-control custom-textfield col-lg-3 col-md-4 col-sm-4">
+                                                <input required min="1982" type="number" class="form-control custom-textfield col-lg-3 col-md-3 col-sm-4">
 
 
 
@@ -150,7 +156,7 @@
                                     </div>
                                     <div class="container1 custom-container-form col-lg-12 col-md-12 col-sm-12 ">
 
-                                        <button type="submit" class="btn btn-primary-add-data margin-custom col-lg-2 col-md-4 col-sm-8">Submit</button>
+                                        <button type="submit" class="btn btn-primary-add-data margin-custom col-lg-2 col-md-4 col-sm-8" >Submit</button>
                                         <a href="/admin/akademik" class="btn btn-primary-download-excel margin-custom col-lg-2 col-md-4 col-sm-8">Batal</a>
                                     </div>
 
@@ -166,5 +172,8 @@
         </div>
     </div>
 </div>
+
+
 <script src="<?= base_url('asset/js/custom-npm-search.js'); ?>"></script>
+
 <?= $this->endSection('content') ?>
