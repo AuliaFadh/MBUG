78a798ae14b8532ef79b0e7aee6a2ec916cdceea
Isa Tarmana <isatarmanamustopa54@gmail.com>
2024-05-26 15:31:18 +0700

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter4/appstarter',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cbf50df0bef4355d80a9ca4932bd17077bd4b403',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codeigniter4/appstarter' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cbf50df0bef4355d80a9ca4932bd17077bd4b403',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
